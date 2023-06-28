d3b8f43fadfcda1925a66bd2928c81739763a433
Joe Reynolds <61364175+jf-grpo@users.noreply.github.com>
2023-06-28 19:30:39 +0000

    studying on 28 June 2023

--- Start/Ch_2/docstrings_start.py
+++ Start/Ch_2/docstrings_start.py
@@ -1,8 +1,16 @@
 # Example file for Advanced Python: Language Features by Joe Marini
 # Demonstrate the use of lambda functions
 
+# To learn more about docstrings > peps.python.org/pep-0257/
 
 def my_function(arg1, arg2=None):
+    # to add a doc funtion
+    """
+    my_funtion(arg1, arg2=None) --> Doesn't really do anything special.
+    Parameters:
+    arg1: the first argument.
+    arg2: the second argument.
+    """
     print(arg1, arg2)
 
 
--- Start/Ch_2/varargs_start.py
+++ Start/Ch_2/varargs_start.py
@@ -3,11 +3,23 @@
 
 
 # TODO: define a function that takes variable arguments
-def addition():
-    pass
+def addition(*args):
+    result = 0
+    for arg in args:
+        result += arg
+    return result
 
 
 # TODO: pass different arguments
-print(addition())
+print(addition(5, 10, 20))
+print(addition(1, 2, 3, 4, 5))
+
 
 # TODO: pass an existing list
+my_nums = [5, 10, 15, 20]
+print(addition(*my_nums))
+
+# drawback to variable argument lists \n
+# If you change the function signature, you have to change all the calls to the function
+# in other words. Adding arguments to the function signature will break the code
+# or at least cause unexpected results/errors
