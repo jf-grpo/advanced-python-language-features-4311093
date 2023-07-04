93f45f1540ef305b5c2cb765e4e45471e606e9e3
Joe Reynolds <61364175+jf-grpo@users.noreply.github.com>
2023-07-04 15:25:25 +0000

    challenge 2 complete

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -13,10 +13,29 @@
 
 
 def string_combiner(*args, unique=False):
-    result = ""
 
     # YOUR CODE HERE
+    """
+    This is a challenge in Advanced Python course
+    this function should do all of the following:
+        1: accepts a variable number of strings and numbers. Other types ignored
+        2: accepts a keyword-only argument to return a unique-only result
+        3: combines all the arguments into a single string
+        4: returns a string containing all arguments combined as one string
+        5: Has a docstring that explains how it works
+        If the unique-only argument is True (default False), then the result
+        combined string will not contain any duplicate characters
+    """
+    result = ""
+    for arg in args:
+        if isinstance(arg, int):
+            result += str(arg)
+        elif isinstance(arg, str):
+            result += arg
 
+    if unique:
+        newresult = set(result)
+        result = "".join(newresult)
     return result
 
 
