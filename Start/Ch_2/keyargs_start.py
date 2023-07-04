36ed15cf0936219b0f01e0a5315d588c13dfe172
Joe Reynolds <61364175+jf-grpo@users.noreply.github.com>
2023-07-04 14:34:44 +0000

    study update July 4

--- Start/Ch_2/keyargs_start.py
+++ Start/Ch_2/keyargs_start.py
@@ -3,10 +3,21 @@
 
 
 # use keyword-only arguments to help ensure code clarity
-def MyFunction():
+# essentially force the caller to supply the arguments by name \
+# so that it is more clear what the code is doing
+
+
+# TODO: define a function that uses keyword-only arguments
+# everything after the '*' requires use of the keyword
+def MyFunction(arg1, arg2, *, suppressExceptions=False):
     pass
 
 
 # try to call the function without the keyword
 # myFunction(1, 2, True)
-MyFunction()
+# MyFunction(1, 2, True) # this will fail \
+# stating that 'MyFunction() takes 2 positional arguments \
+# but 3 were given'.
+
+# The correct way
+MyFunction(1, 2, suppressExceptions=True)  # this will work
--- Start/Ch_2/lambdas_start.py
+++ Start/Ch_2/lambdas_start.py
@@ -14,5 +14,25 @@ ctemps = [0, 12, 34, 100]
 ftemps = [32, 65, 100, 212]
 
 # TODO: Use regular functions to convert temps
+print(list(map(fahrenheit_to_celsisus, ftemps)))
+print(list(map(celsisus_to_fahrenheit, ctemps)))
+
+# lambdas are very useful when you don't really need a named function \
+# just to execute a simple calculation
+
 
 # TODO: Use lambdas to accomplish the same thing
+# Here is what line 17 would look like using a lambda
+# notice the function is now inline as lambda
+# t is the parameter, you could use any name
+print(list(map(lambda t: (t-32) * 5/9, ftemps)))
+# Likewise, here is line 18 using a lambda
+print(list(map(lambda t: (t * 9/5) + 32, ctemps)))
+
+# Otherwise you might end up calling functions from other libraries \
+# just to do a simple calculation. Down the road one would have to \
+# trace back all of those functions to see what they do. \
+# This simplifies the code and makes it easier to read.
+
+# They are not ideal for every situation, but they are very useful \
+# to keep code simple.
